8ac258a50e1211b45f6798cfb67611563fb35d7f
ganciaux <ghislain.anciaux@internet.lu>
2023-05-17 16:55:55 +0200

    Add middleware, user controller and routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
-const port = 5000;
-const app = express();
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("server is ready"));
+
+app.use(notFound);
+app.use(errorHandler);
+
 app.listen(port, () => {
   console.log(`server listen on port ${port}`);
 });
