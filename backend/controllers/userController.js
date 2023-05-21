3e754918542351a28c09ceca2c4cbf580c4b2fa0
ganciaux <ghislain.anciaux@live.fr>
2023-05-21 17:19:45 +0200

    Add users endpoint and mongodb connection

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,4 +4,20 @@ const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth user" });
 });
 
-export { authUser };
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register user" });
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout user" });
+});
+
+const profileUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Profile user" });
+});
+
+const updateUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update user" });
+});
+
+export { authUser, registerUser, logoutUser, profileUser, updateUser };
