7861adaee2c4509dea8ab15f6e52c1be6cc98de5
ganciaux <ghislain.anciaux@live.fr>
2023-05-21 22:03:18 +0200

    Update logout endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -42,7 +42,11 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Logout user' });
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0)
+  });
+  res.status(200).json({ message: 'User Logged out' });
 });
 
 const profileUser = asyncHandler(async (req, res) => {
